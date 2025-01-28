3057f13c50bf796a47b2dd2b5d9f4851cf128b10
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-01-28 15:55:52 -0500

    wrote bad password function

--- A02/password.c
+++ A02/password.c
@@ -1,9 +1,25 @@
 /***************************************************
- * Author: 
+ * Author: Reagan Buvens
  * 
  */
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+  char pwrd[2048];
+  printf("Enter a word: ");
+  scanf("%s", pwrd);
+  for (int i = 0; i < strlen(pwrd); i++) {
+    if (pwrd[i] == 'e') {
+      pwrd[i] = '3';
+    }
+    else if (pwrd[i] == 'l') {
+      pwrd[i] = '1';
+    }
+    else if (pwrd[i] == 'a') {
+      pwrd[i] = '@';
+    }
+  }
+  printf("Your bad password is %s\n", pwrd);
   return 0;
 }
