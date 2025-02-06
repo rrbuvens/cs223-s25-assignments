c9d66f43c8a45f92b3764fc8206c87a0cd736de8
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-02-06 15:16:41 -0500

    Code Jam 1 completed

--- J01/countsort.c
+++ J01/countsort.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-   return 0;
+   char digits[32];
+
+   printf("Enter digits: ");
+   scanf("%s", digits);
+
+   int sorted[10];
+   for (int i = 0; i < 10; i++) {
+      sorted[i] = 0;
+   }
+
+   for (int i = 0; i < strlen(digits); i++) {
+      if (digits[i] >= '0' && digits[i] <= '9') {
+         sorted[digits[i] - '0'] += 1;
+      }
+   }
+
+   for (int i = 0 ; i < 10; i++) {
+      for (int j = 0; j < sorted[i]; j++) {
+         printf("%d ", i);
+      }
+   }
 }
--- J01/price.c
+++ J01/price.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-   return 0;
+   int guess;
+
+   printf("Welcome to the Price is Right!\nEnter a guess to win a luxurious new dishwasher: $");
+   scanf("%d", &guess);
+
+   int ai1, ai2, price;
+
+   srand(time(0));
+
+   price = (rand() % 2001) + 3000;
+   ai1 = (rand() % 2001) + 3000;
+   ai2 = (rand() % 2001) + 3000;
+
+   printf("AI contestant #1 guesses $%d\nAI contestant #2 guesses $%d\n", ai1, ai2);
+   printf ("The dishwasher cost $%d\n", price);
+
+   if (guess > price && ai1 > price && ai2 > price) {
+      printf("Sorry. No one wins the dishwasher. I'm keeping it.\n");
+   }
+   else if (guess <= price && ((guess > ai1 && guess > ai2) || (ai1 > price && ai2 > price)
+      || (guess > ai1 && ai2 > price) || (guess > ai2 && ai1 > price))) {
+      printf("Congratulations! You won the dishwasher!\n");
+   }
+   else if (ai1 <= price && ((ai1 > guess && ai1 > ai2) || (guess > price && ai2 > price)
+      || (ai1 > guess && ai2 > price) || (ai1 > ai2 && guess > price))) {
+      printf("AI contestant #1 wins the dishwasher!\n");
+   }
+   else {
+      printf("AI contestant #2 wins the dishwasher!\n");
+   }
 }
 
