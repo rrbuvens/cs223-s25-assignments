d63aae3ccbf106d3e0a43b640c319791c92f4abb
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-02-28 21:20:17 -0500

    implemented stack methods

--- A06/match_braces.c
+++ A06/match_braces.c
@@ -1,7 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct brace {
+  char data;
+  int line;
+  int column;
+  struct brace* next;
+};
+
+struct brace* push(char data, int line, int column, struct brace* stack) {
+  struct brace* top = (struct brace*) malloc(sizeof(struct brace));
+  if (!top) {
+    printf("Memory allocation error. Exiting...");
+    exit(1);
+  }
+  top->data = data;
+  top->line = line;
+  top->column = column;
+  top->next = stack;
+  return top;
+}
+
+struct brace* pop(struct brace* popped, struct brace* stack) {
+  if (stack) {
+    struct brace* temp = stack;
+    stack = stack->next;
+    *popped = *temp;
+    free(temp);
+    return stack;
+  }
+  else {
+    printf("Nothing to pop.");
+    return NULL;
+  }
+}
+
+void clearStack(struct brace* stack) {
+  while (stack) {
+    struct brace* temp = stack;
+    stack = stack->next;
+    free(temp);
+  }
+}
+
+void printStack(struct brace* stack) {
+  struct brace* ptr = stack;
+  int n = 0;
+  while (ptr) {
+    printf("%d) char:%c\tline: %d\tcolumn: %d\n", n, ptr->data, ptr->line, ptr->column);
+    ptr = ptr->next;
+    n++;
+  }
+}
 
 int main()
 {
+  
+
   return 0;
 }
 
