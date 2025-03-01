400b37c2e06df5d9100172421a293dca9a5b3089
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-02-28 23:05:28 -0500

    implemented match_braces

--- A06/match_braces.c
+++ A06/match_braces.c
@@ -1,3 +1,9 @@
+/*
+ * Name: Reagan Buvens
+ * Date: 02/28/2025
+ * Description: Given an input file, checks that all braces are matched.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,7 +17,7 @@ struct brace {
 struct brace* push(char data, int line, int column, struct brace* stack) {
   struct brace* top = (struct brace*) malloc(sizeof(struct brace));
   if (!top) {
-    printf("Memory allocation error. Exiting...");
+    printf("Memory allocation error. Exiting...\n");
     exit(1);
   }
   top->data = data;
@@ -21,16 +27,14 @@ struct brace* push(char data, int line, int column, struct brace* stack) {
   return top;
 }
 
-struct brace* pop(struct brace* popped, struct brace* stack) {
+struct brace* pop(struct brace** popped, struct brace* stack) {
   if (stack) {
-    struct brace* temp = stack;
+    *popped = stack;
     stack = stack->next;
-    *popped = *temp;
-    free(temp);
     return stack;
   }
   else {
-    printf("Nothing to pop.");
+    *popped = NULL;
     return NULL;
   }
 }
@@ -53,9 +57,54 @@ void printStack(struct brace* stack) {
   }
 }
 
-int main()
-{
-  
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("usage: ./match_braces input_file\n");
+    exit(1);
+  }
+
+  FILE* fp = fopen(argv[1], "r");
+  if (!fp) {
+    printf("File error. Exiting...\n");
+    exit(1);
+  }
+
+  char ch = fgetc(fp);
+  int l = 1;
+  int c = 0;
+  struct brace* braces = NULL;
+  while(ch != EOF) {
+    if (ch == '{') {
+      braces = push(ch, l, c, braces);
+    }
+    else if (ch == '}') {
+      struct brace* popped = NULL;
+      braces = pop(&popped, braces);
+      if (!popped) {
+        printf("Unmatched brace on Line %d and Column %d\n", l, c);
+      }
+      else {
+        printf("Found matching braces: (%d, %d) -> (%d, %d)\n", popped->line, popped->column, l, c);
+        free(popped);
+      }
+    }
+    else if (ch == '\n') {
+      l++;
+      c = 0;
+    }
+    c++;
+    ch = fgetc(fp);
+  }
+
+  while (braces) {
+    struct brace* popped = NULL;
+    braces = pop(&popped, braces);
+    printf("Unmatched brace on Line %d and Column %d\n", popped->line, popped->column);
+    free(popped);
+  }
+
+  fclose(fp);
+  clearStack(braces);
 
   return 0;
 }
