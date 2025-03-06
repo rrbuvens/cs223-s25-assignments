4bcf68e60cbf9a12dffd3d5f84ce525448d28bad
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-03-05 20:12:40 -0500

    implemented bitmap.c

--- A07/bitmap.c
+++ A07/bitmap.c
@@ -1,7 +1,8 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Reagan Buvens
+ * Date: 03/05/2025
+ * Description: Given a 64-bit unsigned integer, produces an 8x8 ascii image
+ * representing each bit of the integer.
  ---------------------------------------------*/
 #include <stdio.h>
 
@@ -10,6 +11,18 @@ int main() {
   scanf(" %lx", &img);
   printf("Image (unsigned long): %lx\n", img);
 
-  // todo: your code here
+  for (int i = 63; i >= 0; i--) {
+    unsigned long mask = 0x1ul << i;
+    int bit = (mask & img) >> i;
+    if (bit) {
+      printf("@ ");
+    }
+    else {
+      printf("  ");
+    }
+    if (i % 8 == 0) {
+      printf("\n");
+    }
+  }
   return 0;
 }
