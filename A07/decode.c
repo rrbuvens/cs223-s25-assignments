ffa2d7be29b74864745b0f228ca5d51eb1c6dbb0
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-03-06 23:53:58 -0500

    implemented decode.c

--- A07/decode.c
+++ A07/decode.c
@@ -1,7 +1,8 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Reagan Buvens
+ * Date: 03/05/2025
+ * Description: Given a ppm file, decodes any possible message which might be hiding
+ * in the least significant bits.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +13,39 @@ int main(int argc, char** argv) {
     printf("usage: decode <file.ppm>\n");
     return 0;
   }
+
+  int w, h;
+  struct ppm_pixel** pixels = read_ppm_2d(argv[1], &w, &h);
+  printf("Reading %s with width %d and height %d\n", argv[1], w, h);
+  printf("Max number of characters in the image: %d\n", (w*h*3)/8);
+
+  unsigned char *colors = (unsigned char*) malloc(sizeof(unsigned char) * 3 * w * h);
+  int p = 0;
+  for (int i = 0; i < w; i++) {
+    for (int j = 0; j < h; j++) {
+      colors[p++] = pixels[i][j].red;
+      colors[p++] = pixels[i][j].green;
+      colors[p++] = pixels[i][j].blue;
+    }
+  }
+
+  unsigned char curr = 0;
+  for (int i = 0; i < w * h * 3; i++) {
+    curr += ((0x1 & colors[i]) << (7 - (i % 8)));
+    if ((i + 1) % 8 == 0) {
+      if (curr == '\0') { break; }
+      else { 
+        printf("%c", curr);
+        curr = 0;
+      }
+    }
+  }
+
+  for (int i = 0; i < w; i++) {
+    free(pixels[i]);
+  }
+  free(pixels);
+  free(colors);
 
   return 0;
 }
--- A07/write_ppm.c
+++ A07/write_ppm.c
@@ -1,18 +1,38 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Reagan Buvens
+ * Date: 2/22/2025
+ * Description: Takes in a filename, a 2d array of pixels, and the dimensions
+ * of that array. Writes the contents of the array to a file with the given
+ * name in binary.
  ---------------------------------------------*/
-#include "write_ppm.h"
-#include <stdio.h>
-#include <string.h>
-
-// implement *one* (but do not remove the other one)!
-
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
-
-}
-
-void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
-
-}
+ #include "write_ppm.h"
+ #include <stdio.h>
+ #include <string.h>
+ 
+ // implement *one* (but do not remove the other one)!
+ 
+ void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+ 
+ }
+ 
+ void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+     FILE *fp = fopen(filename, "wb");
+     if (fp == NULL) {
+         printf("File error. Exiting...");
+         return;
+     }
+ 
+     fprintf(fp, "P6\n");
+     fprintf(fp, "# Created by write_ppm_2d\n");
+     fprintf(fp, "%d %d\n", w, h);
+     fprintf(fp, "255\n");
+ 
+     for (int i = 0; i < h; i++) {
+         for (int j = 0; j < w; j++) {
+             fwrite(&pixels[i][j], sizeof(struct ppm_pixel), 1, fp);
+         }
+     }
+ 
+     fclose(fp);
+ }
+ 
