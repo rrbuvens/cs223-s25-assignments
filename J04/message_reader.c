83a2ff309809633d45e53f6a192a4174a4a5709c
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-03-25 15:25:16 -0400

    jam 4 completed

--- J04/message_reader.c
+++ J04/message_reader.c
@@ -1,4 +1,41 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct meta_data {
+  float version;
+  unsigned int year;
+  unsigned int length;
+};
+
+int main(int argc, char **argv)
 {
+  if (argc != 2) {
+    printf("usage: ./message_reader filename\n");
+    return 0;
+  }
+  FILE *fp = fopen(argv[1], "rb");
+  if (!fp) {
+    printf("Invalid filename. Exiting...\n");
+    return 0;
+  }
+
+  char line[256];
+  fgets(line, 256, fp); //read comment line
+  printf("Comment: %s\n", line);
+
+  struct meta_data *data = (struct meta_data*) malloc(sizeof(struct meta_data));
+  fread(data, sizeof(struct meta_data), 1, fp);
+  printf("Version: %0.2f\nYear: %u\nLength: %u\n", data->version, data->year, data->length);
+
+  char *chars = (char*) malloc(sizeof(char) * (data->length + 1));
+  for (int i = 0; i < data->length + 1; i++) {
+    fread(&chars[i], sizeof(char), 1, fp);
+  }
+  printf("Message: %s\n", chars);
+
+  free(data);
+  free(chars);
+  fclose(fp);
   return 0;
 }
--- J04/message_writer.c
+++ J04/message_writer.c
@@ -1,4 +1,36 @@
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct meta_data {
+  float version;
+  unsigned int year;
+  unsigned int length;
+};
+
+int main(int argc, char **argv)
 {
+  if (argc != 2) {
+    printf("usage: ./message_writer filename\n");
+    return 0;
+  }
+
+  FILE *fp = fopen(argv[1], "wb");
+  if (!fp) {
+    printf("Invalid filename. Exiting...\n");
+    return 0;
+  }
+
+  fprintf(fp, "# This is a comment\n");
+
+  struct meta_data data = {0.15, 2025, 46};
+  fwrite(&data, sizeof(struct meta_data), 1, fp);
+
+  char message[47] = "Hello! This is my message! Have a nice day. :)";
+  for (int i = 0; i < strlen(message) + 1; i++) {
+    fwrite(&message[i], sizeof(char), 1, fp);
+  }
+
+  fclose(fp);
   return 0;
 }
