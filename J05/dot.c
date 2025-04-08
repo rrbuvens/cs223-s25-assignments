dc34d8256f7a7057d6fe992d36b75530f5599ee4
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-04-08 15:48:52 -0400

    code jam 5 completed

--- J05/dot.c
+++ J05/dot.c
@@ -5,28 +5,56 @@
 #include <pthread.h>
 
 #define SIZE 100000
-
 // your code here
-
+struct thread_args {
+  int *u;
+  int *v;
+  int start;
+  int end;
+  int dot_product;  // This will store the partial result
+};
+// Thread function
+void* thread_dot(void* arg) {
+  struct thread_args* data = (struct thread_args*) arg;
+  int sum = 0;
+  for (int i = data->start; i < data->end; i++) {
+    sum += data->u[i] * data->v[i];
+  }
+  data->dot_product = sum;  // Store result in struct field
+  return NULL;
+}
 int main(int argc, char *argv[]) {
   srand(time(0));
-
   int v[SIZE];
   int u[SIZE];
   int dotproduct = 0;
-   
+  // Initialize vectors and compute ground truth
   for (int i = 0; i < SIZE; i++) {
     v[i] = rand() % 1000 - 500;
     u[i] = rand() % 1000 - 500;
     dotproduct += u[i] * v[i];
   }
   printf("Ground truth dot product: %d\n", dotproduct);
-
-  // TODO: Implement your thread solution here
-  int thread_dotproduct = 0;
+  // Create threads
   printf("Test with 4 threads\n");
+  pthread_t threads[4];
+  struct thread_args args[4];
+  for (int i = 0; i < 4; i++) {
+    args[i].start = i * 250;
+    args[i].end = (i == 4 - 1) ? SIZE : (i + 1) * 250;
+    args[i].u = u;
+    args[i].v = v;
+    args[i].dot_product = 0;
+    pthread_create(&threads[i], NULL, thread_dot, &args[i]);
+  }
+
+  int thread_dotproduct = 0;
+  
+  for (int i = 0; i < 4; i++) {
+    pthread_join(threads[i], NULL);
+    thread_dotproduct += args[i].dot_product;
+  }
 
   printf("Answer with threads: %d\n", thread_dotproduct);
-
   return 0;
 }
--- J05/multiply.c
+++ J05/multiply.c
@@ -3,10 +3,25 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include <pthread.h>
-
 #define SIZE 100
-
 // your code here
+struct thread_args {
+  int *m;
+  int *u;
+  int start;
+  int end;
+  int* product;
+};
+
+void* thread_multiply(void* arg) {
+  struct thread_args* data = (struct thread_args*) arg;
+  for (int i = data->start; i < data->end; i++) {
+    for (int j = 0; j < 100; j++) {
+      data->product[i] += data->u[j] * data->m[i * 100 + j]; 
+    }
+  }
+  return NULL;
+}
 
 int main(int argc, char *argv[]) {
   srand(time(0));
@@ -15,10 +30,11 @@ int main(int argc, char *argv[]) {
   int u[SIZE];
   int result[SIZE];
   int result_threads[SIZE];
-   
+
   for (int i = 0; i < SIZE; i++) {
     u[i] = rand() % 10 - 5;
     result[i] = 0;
+    result_threads[i] = 0;
   }
 
   for (int i = 0; i < SIZE; i++) {
@@ -31,14 +47,31 @@ int main(int argc, char *argv[]) {
 
   // TODO: Implement your thread solution here
   printf("Test with 4 threads\n");
+  pthread_t threads[4];
+  struct thread_args args[4];
 
+  for (int i = 0; i < 4; i++) {
+    args[i].start = i * 25;
+    args[i].end = (i == 4 - 1) ? SIZE : (i + 1) * 25;
+    args[i].m = M;
+    args[i].u = u;
+    args[i].product = result_threads;
+    pthread_create(&threads[i], NULL, thread_multiply, (void *) &args[i]);
+  }
 
+  for (int i = 0; i < 4; i++) {
+    pthread_join(threads[i], NULL);
+    // product += args[i].product;
+    for (int j = i * 250; j < (i+1)*250; j++) {
+      result_threads[j] = args[i].product[j];
+    }
+  }
 
   int error = 0;
   for (int i = 0; i < SIZE; i++) {
     error += abs(result[i] - result_threads[i]);
   }
-  printf("Error between ground truth and multithreaded version: %d\n", error);
 
+  printf("Error between ground truth and multithreaded version: %d\n", error);
   return 0;
 }
