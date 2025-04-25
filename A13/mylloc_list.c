1b0b49414e0f84ea2526a37a6f07fac2a48103e0
Reagan Riley Buvens <rbuvens3@goldengate.cs.brynmawr.edu>
2025-04-25 18:36:00 -0400

    assignment 13 completed

--- A13/mylloc_list.c
+++ A13/mylloc_list.c
@@ -1,3 +1,10 @@
+/**
+ * Name: Reagan Buvens
+ * Date: 04/25/2025
+ * Description: Implements malloc and free using a free list, such that
+ * previously allocated memory space may be reused after being freed.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,10 +18,48 @@ struct chunk *flist = NULL;
 
 void *malloc (size_t size) {
   // TODO: Implement malloc with a free list (See reading for details)
-  return NULL;
+  if (size == 0) {
+    return NULL;
+  }
+  struct chunk *next = flist;
+  struct chunk *prev = NULL;
+
+  while (next != NULL) {
+    if (next->size >= size) {
+      next->used = size;
+      if (prev != NULL) {
+        prev->next = next->next;
+      }
+      else {
+        flist = next->next;
+      }
+      return (void*)(next + 1);
+    }
+    else {
+      prev = next;
+      next = next->next;
+    }
+  }
+
+  void *memory = sbrk(size + sizeof(struct chunk));
+  if (memory == (void *) - 1) {
+    return NULL;
+  }
+  else {
+    struct chunk *cnk = (struct chunk*) memory;
+    cnk->size = size;
+    cnk->used = size;
+    return (void *)(cnk + 1);
+  }
 }
 
 void free(void *memory) {
   // TODO: Implement malloc with a free list (See reading for details)
+  if (memory != NULL) {
+    struct chunk *cnk = (struct chunk*) ((struct chunk*)memory - 1);
+    cnk->next = flist;
+    cnk->used = 0;
+    flist = cnk;
+  }
   return;
 }
